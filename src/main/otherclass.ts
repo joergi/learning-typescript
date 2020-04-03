9f990bd07008bb37bdbab35f45d421422c9019a1
joergi <1439809+joergi@users.noreply.github.com>
2020-04-03 18:50:10 +0200

    code with error - for testing the github actions

--- src/main/otherclass.ts
+++ src/main/otherclass.ts
@@ -1,6 +1,8 @@
+import {logger} from "./logger";
+
 // the private function, can't be called from outside
 function privateFunction(someString: String) {
-    console.log("this is coming from outside: " + someString);
+    logger.info("this is coming from outside: " + someString
 }
 
 // the public function, it can be called from the outside.
