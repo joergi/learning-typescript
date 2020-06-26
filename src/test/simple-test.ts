0ce5aa8c34d62bb692dc10ead2d83a16e1443d77
joergi <1439809+joergi@users.noreply.github.com>
2020-06-26 12:15:05 +0200

    committing a failing test to see travis is breaking

--- src/test/simple-test.ts
+++ src/test/simple-test.ts
@@ -3,6 +3,6 @@ import { expect } from "chai";
 describe('calculate', function() {
     it('add', function() {
         let result = 7+2;
-        expect(result).equal(9);
+        expect(result).equal(7);
     });
 });
